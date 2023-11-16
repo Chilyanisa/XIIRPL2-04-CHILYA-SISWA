766847b2d467db192d1721e3906b2bfa75353b4a
ACER <chilyafadilatinisa0505@gmail.com>
2023-11-16 10:52:08 +0700

    first commit

--- controllers/user.js
+++ controllers/user.js
@@ -40,7 +40,7 @@ module.exports = {
         }
        
       },
-      store: async (req, res) => {
+      update: async (req, res) => {
         try {
             const user = await User.create(req.body)
             res.status(200).json({
