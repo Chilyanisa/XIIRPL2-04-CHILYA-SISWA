353a80fb884fba84e36f07f67ba5b979a8ca6ac0
ACER <chilyafn@gmail.com>
2023-11-16 08:54:34 +0700

    first commit

--- router/users.js
+++ router/users.js
@@ -5,6 +5,8 @@ const usercontroller = require('../controllers/user')
 
 router.get('/users', usercontroller.index)
 
+router.get('/users/:id', usercontroller.index)
+
 router.post('/users', usercontroller.store) 
 
 router.put('/user/:id', usercontroller.update)
